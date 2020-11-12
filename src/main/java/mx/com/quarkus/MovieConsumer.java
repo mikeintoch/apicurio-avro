d21c4cb2c2fee2a5a87916c09e02548ebbc89f1f
Miguel Delgadillo <mdelgadi@redhat.com>
2020-11-12 12:34:21 -0600

    First Commit

---
+++ src/main/java/mx/com/quarkus/MovieConsumer.java
@@ -0,0 +1,21 @@
+package mx.com.quarkus;
+
+import mx.com.quarkus.schema.Movie;
+import org.eclipse.microprofile.reactive.messaging.Incoming;
+import org.jboss.logging.Logger;
+
+import javax.enterprise.context.ApplicationScoped;
+
+@ApplicationScoped
+public class MovieConsumer {
+
+    private static final Logger LOGGER =
+        Logger.getLogger("MovieConsumer");
+
+    @Incoming("movies-from-kafka")
+    public void receive(Movie movie) {
+        LOGGER.infof("Received movie: %s (%d)",
+            movie.getTitle(), movie.getYear());
+    }
+
+}
